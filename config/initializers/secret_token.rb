3736cc7697eea97c641762cfaefe1c4b7d5651c9
Andrej Hoos <andrej.hoos@gmail.com>
2014-02-17 19:36:49 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpotTheBin::Application.config.secret_key_base = '272867a81f0c840b2a5b450d717e858302319f11935e73dac77db153b9f0d65e68ab63ac77f3c7940c46dfa5e4ccdf330cb443259ba86fef5d2f9a14eb9c82e3'
