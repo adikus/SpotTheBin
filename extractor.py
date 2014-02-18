a3d43e14abab7e84b450144b58b338922122b9cc
SimonRovder <simon.rovder@gmail.com>
2014-02-18 20:22:10 +0000

    dsdvsfdvb

---
+++ extractor.py
@@ -0,0 +1,30 @@
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+lines = [line.strip() for line in open('Source.txt')]
+file = open("result", 'w')
+category = "Libraries"
+for line in lines :
+	line = line.replace("\"", "")
+	line = line.replace("\'", "")
+	line = line.replace("\n", "")
+	data = line.split(",")
+	if len(data) > 3 :
+		name = data[1].strip()
+		name = (name[:75] + '..') if len(name) > 75 else name
+		print line
+		y = data[len(data) - 2].strip()
+		x = data[len(data) - 1].strip()
+		if ((name != "") and (x != "") and (y != "") and (is_number(x)) and (is_number(y))) :
+			file.write("{")
+			file.write("name:\"" + name + "\",")
+			file.write("y:" + y + ",")
+			file.write("x:" + x + ",")
+			file.write("category: \"" + category + "\"")
+			file.write("},")
+
