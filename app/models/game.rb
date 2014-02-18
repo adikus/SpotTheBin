165723062c8cab77d1ce0c441c29da6b1ef00357
SimonRovder <simon.rovder@gmail.com>
2014-02-18 14:56:10 +0000

    Tralala

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ActiveRecord::Base
 
   validates :name, :password, :start_time, presence: true
+  has_many :nodes
+  has_many :players
 
 end
--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,7 @@
 class Node < ActiveRecord::Base
+
+	belongs_to :game
+	belongs_to :player
+	belongs_to :place
+
 end
