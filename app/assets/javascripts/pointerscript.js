c0eb961964b9f418418226b47cee5c7e7bb35c65
Andrej Hoos <andrej.hoos@gmail.com>
2014-02-18 13:33:15 +0000

    Nicify stuff

--- app/assets/javascripts/pointerscript.js
+++ app/assets/javascripts/pointerscript.js
@@ -1,40 +1,8 @@
-function FindPosition(oElement)
-{
-  if(typeof( oElement.offsetParent ) != "undefined")
-  {
-    for(var posX = 0, posY = 0; oElement; oElement = oElement.offsetParent)
-    {
-      posX += oElement.offsetLeft;
-      posY += oElement.offsetTop;
-    }
-      return [ posX, posY ];
-    }
-    else
-    {
-      return [ oElement.x, oElement.y ];
-    }
-}
-function GetCoordinates(e)
-{
-  var PosX = 0;
-  var PosY = 0;
-  var ImgPos;
-  ImgPos = FindPosition(myImg);
-  if (!e) var e = window.event;
-  if (e.pageX || e.pageY)
-  {
-    PosX = e.pageX;
-    PosY = e.pageY;
-  }
-  else if (e.clientX || e.clientY)
-    {
-      PosX = e.clientX + document.body.scrollLeft
-        + document.documentElement.scrollLeft;
-      PosY = e.clientY + document.body.scrollTop
-        + document.documentElement.scrollTop;
-    }
-  PosX = PosX - ImgPos[0];
-  PosY = PosY - ImgPos[1];
-  document.getElementById("x").innerHTML = PosX;
-  document.getElementById("y").innerHTML = PosY;
-}
+$(function(){
+    $('#pointer').click(function(e){
+        var imgPos = $(this).offset();
+
+        $('#x').text(Math.round(e.pageX - imgPos.left));
+        $('#y').text(Math.round(e.pageY - imgPos.top));
+    });
+});
